a3937ab243e068645cacbdbe822f1705fb1b9b80
RunshanHu <runsh.hu@gmail.com>
2017-07-30 20:58:35 +0100

    add promise in controllers

--- InterfaceAPI/api/controllers/AnonyInterface.js
+++ InterfaceAPI/api/controllers/AnonyInterface.js
@@ -1,12 +1,40 @@
 'use strict';
 
-module.exports = {register, queryFromUser, queryFromRI, returnFinalResult};
+var request = require('request-promise');
+
+module.exports = {register, queryFromUser};
+
+var registerEventHandler = {
+
+};
+
+var queryEventHandler = {
+   queryRI: function(req) {
+    // send the query to Registry Interface
+     
+   },
+
+  getResultFromAnonyService: function(budget) {
+    // query Anonymisation service using budget
+    
+  },
+
+  sendResultToRI: function() {
+    // send the anonymised result to Registry Interface
+    
+  },
+
+  sendFinalResToUser: function() {
+    // send the final result to the user
+    
+  }
+
+};
 
 function register(req, res, next) {
   /*
    * register a new data sharing event
    */
-
 
 }
 
@@ -14,26 +42,10 @@ function queryFromUser(req, res, next){
   /*
    * process a user query  
    */
-  
-  var old_res = queryRI();
-  if(old_res == true) {
-      var perturbed_res = getResFromAnonyService();
-      var utility_check = sendResToRI(perturbed_res);
-       
-
-
-  }
+   res = queryEventHandler.queryRI(req)
+    .then(getResultFromAnonyService)
+    .then(sendResultToRI)
+    .then();
 }
 
-function queryFromRI(req, res, next) {
-  /*
-   * process the query from RI 
-   */
 
-} 
-
-function returnFinalResult(req, res, next) {
-  /*
-   * return final result to User
-   */
-}
