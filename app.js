b667edfd41eb989e50ea69f6e75c3e30b62251cf
RunshanHu <runsh.hu@gmail.com>
2017-08-23 20:38:04 +0100

    use config file to pass the parameter for server setting

--- app.js
+++ app.js
@@ -2,11 +2,15 @@
 
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
+var yaml_config = require('node-yaml-config');
+
 module.exports = app; // for testing
 
 var config = {
-  appRoot: __dirname // required config
+  appRoot: __dirname, // required config
 };
+
+var configuration = yaml_config.load(__dirname + '/config/server_config.yaml');
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
@@ -14,9 +18,11 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   // install middleware
   swaggerExpress.register(app);
 
-  var port = process.env.PORT || 50005;
+  //var server_setting = configuration.server_setting;
+  var port = process.env.PORT || configuration.server.port;
   app.listen(port);
   
-  console.log('Anonymisation interface started! Listening on http://localhost:%d', port);
+  console.log('Anonymisation interface started! Listening on http://%s:%d', 
+              configuration.server.host, port);
 
 });
